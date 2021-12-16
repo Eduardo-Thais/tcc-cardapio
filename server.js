727d30b7ee3bca367fb95ce7bec128053c28096e
Eduardo de Souza Santos Thais <dudu2541999@gmail.com>
2021-12-16 02:11:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,8 @@
-
-
 const express = require('express')
-const path    = require('path')
-const cors = require("cors")
-
-
-
-
+const serveStatic = require('serve-static')
+const path = require('path')
 
 const app = express()
-
-app.use(staticFileMiddleware)
-app.use(cors())
-
-
-const serveStatic = require('serve-static')
-
 
 //here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
@@ -28,13 +15,3 @@ app.get(/.*/, function (req, res) {
 const port = process.env.PORT || 8080
 app.listen(port)
 console.log(`app is listening on port: ${port}`)
-
-
-
-app.use(function(req, res, next){
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-})
